fa74241ea21c7b166c8ce612dd83b3588b1b3996
moheeb1 <61311497+moheeb1@users.noreply.github.com>
2020-02-29 13:46:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,12 +18,12 @@ client.on("message", message => {
 
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
+      console.log(`Desert Bot- Script By : moheeb`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : moheeb ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`S2ud`,"http://twitch.tv/S-F")
+client.user.setGame(``,"")
 client.user.setStatus("dnd")
 });
 
